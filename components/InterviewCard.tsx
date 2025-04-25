bb42779cf5f53394dea6a6b83c51af44805ad9d8
jmoriwa <112183823+jmoriwa@users.noreply.github.com>
2025-04-25 01:48:12 -0500

    Interview cards components

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import dayjs from "dayjs";
 import {date} from "zod";
 import Image from "next/image";
-import {getRandomInterviewCover} from "@/public/utils";
+import {getRandomInterviewCover} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
 
 const InterviewCard = ({interviewId,userId, role, type, techstack,createdAt} : InterviewCardProps) => {
     const feedback = null as Feedback | null;
@@ -18,9 +21,37 @@ const InterviewCard = ({interviewId,userId, role, type, techstack,createdAt} : I
                     </div>
                     <Image  src={getRandomInterviewCover()} alt="cover image" width={90} height={90} className="
                         rounded-full object-fit size-[90px]" />
+                    <h3 className="mask-b-to-5 capitalize">
+                        {role} Interview
+                    </h3>
+                    <div className="flex flex-row gap-5 mt-3">
+                        <div className="flex flex-row gap-2">
+                            <Image src="/calender.svg" alt="calender" width={22} height={22} />
+                            <p>{formattedDate}</p>
+                        </div>
+                        <div className="flex flex-row gap-2 items-center">
+                            <Image src="/star.svd" alt="star" width={22} height={22} />
+                            <p>{feedback?.totalScore || '---'}/100</p>
+                        </div>
+                    </div>
 
+                    <p className="line-clamp-2 mt-5">
+                        {feedback?.finalAssessment ||"You haven't taken the interview yet." +
+                            " Take it now to improve your skills."}
+                    </p>
                 </div>
 
+                <div className="flex flex-row justify-between">
+                    <DisplayTechIcons techStack={techstack} />
+                    <Button className="btn-primary">
+                        <Link  href={feedback
+                        ? `/interview/${interviewId}/feedback`
+                        : `/interview/${interviewId}`
+                        }>
+                            {feedback ? 'Check Feedback' : 'View Interview'}
+                        </Link>
+                    </Button>
+                </div>
             </div>
         </div>
     )
